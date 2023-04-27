62faa13570182b9284dc2368e54e4419496d34e4
AlessioAbbati <alex_117@live.it>
2023-04-27 18:14:07 +0200

    add today's date

--- myscript.js
+++ myscript.js
@@ -3,7 +3,14 @@
 let countDownDate = new Date("April 28, 2023 09:30:00").getTime();
 
 // faccio scorrere il conto alla rovescia di 1 secondo alla volta
+
 let countDown = setInterval(function () {
 
+    // aggiungo data e ora di oggi
+    let now = new Date().getTime();
 
 }, 1000);
+
+
+
+
