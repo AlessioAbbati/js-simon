e078bf5f574880e8132a5497abdbdb80cf8cf6d1
AlessioAbbati <alex_117@live.it>
2023-04-27 18:08:23 +0200

    add countdown

--- myscript.js
+++ myscript.js
@@ -2,28 +2,8 @@
 
 let countDownDate = new Date("April 28, 2023 09:30:00").getTime();
 
-// Update the count down every 1 second
-let x = setInterval(function() {
+// faccio scorrere il conto alla rovescia di 1 secondo alla volta
+let countDown = setInterval(function () {
 
-    // Get today's date and time
-    let now = new Date().getTime();
-      
-    // Find the distance between now and the count down date
-    let distance = countDownDate - now;
-      
-    // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-    // Output the result in an element with id="demo"
-    document.getElementById("count").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-      
-    // If the count down is over, write some text 
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("count").innerHTML = "EXPIRED";
-    }
-  }, 1000);
+
+}, 1000);
